05b419f2e093ac4189fa3cc2cc369795b556db0c
Kyle Conroy <kyleconroy@gmail.com>
2019-08-02 09:23:13 -0400

    implement plot_func_from_dists

--- npdists/npdists.py
+++ npdists/npdists.py
@@ -17,6 +17,13 @@ else:
     _has_corner = True
 
 try:
+    from scipy.stats import norm as _norm
+except ImportError:
+    _has_scipy = False
+else:
+    _has_scipy = True
+
+try:
     from astropy import units as _units
 except ImportError:
     _has_astropy = False
@@ -145,6 +152,59 @@ def logp_from_dists(dists, values):
 
     return logp
 
+def plot_func_from_dists(dists, func, x, N=1000, func_kwargs={}, show=False):
+    """
+    Draw samples and a callable function and plot.
+
+    The passed callable `func` will be called with arguments `x` followed by
+    the individually drawn values from each distribution in `dists` (in order
+    provided) and then any additional `func_kwargs`.
+
+    See also:
+    * <npdists.sample_from_dists>
+
+    Arguments
+    -----------
+    * `dists` (list or tuple of distribution objects): distribution objects from
+        which to sample.
+    * `func` (callable): callable function
+    * `x` (array like): x values to pass to `func`.
+    * `N` (int, optional, default=1000): number of samples to draw.
+    * `func_kwargs` (dict, optional): additional keyword arguments to pass to
+        `func`.
+    * `show` (bool, optional, default=False): whether to call plt.show()
+
+    Returns
+    -----------
+    * list of created matplotlib artists
+
+    Raises
+    -----------
+    * ImportError: if matplotlib is not imported
+    * ImportError: if scipy is not imported
+    """
+
+
+    if not _has_mpl:
+        raise ImportError("plot_from_dists requires matplotlib.")
+    if not _has_scipy:
+        raise ImportError("plot_from_dists requires scipy.")
+
+    # TODO: allow passing arges to sample_from_dists
+    models = _np.array([func(x, *sample_from_dists(dists), **func_kwargs) for i in range(N)])
+
+    # TODO: allow options for sigma boundaries
+    bounds = _np.percentile(models, 100 * _norm.cdf([-2, -1, 1, 2]), axis=0)
+
+    ret1 = _plt.fill_between(x, bounds[0, :], bounds[-1, :],
+                     label="95\% uncertainty", fc="#03A9F4", alpha=0.4)
+    ret2 = _plt.fill_between(x, bounds[1, :], bounds[-2, :],
+                     label="68\% uncertainty", fc="#0288D1", alpha=0.4)
+
+    if show:
+        _plt.show()
+
+    return ret1, ret2
 
 
 ################## VALIDATORS ###################
